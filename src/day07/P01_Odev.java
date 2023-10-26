bc1b252c273d52948fef635e03d58ec2f63b9523
denizyuksl <yukseldnz.33@gmail.com>
2023-10-26 21:11:19 +0300

    t127practise

--- src/day07/P01_Odev.java
+++ src/day07/P01_Odev.java
@@ -18,4 +18,6 @@ public class P01_Odev {
 
         }
     }
+
+
 }
---
+++ src/day08/P05.java
@@ -0,0 +1,16 @@
+package day08;
+
+public class P05 {
+
+    public static void main(String[] args) {
+        /*
+        soru-1:
+       Kullanicidan istedigi kadar isim alip, Q'ya bastiginda girdigi isimleri bize
+liste olarak dondurecek bir method olusturun.
+
+
+        soru-2:Kullanicidan pozitif bir tamsayi alip, o tamsayiyi tam bolebilen tum pozitif
+tamsayilari bir liste olarak bize donduren bir method olusturun.
+         */
+    }
+}
